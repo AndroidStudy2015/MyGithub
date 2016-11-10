0699eb0cf1f8e88121714e905048dc6a28fc3b23
LemonTree86 <android_study_2015@163.com>
2016-11-10 11:23:57 +0800

    frefrf

--- app/src/main/java/com/git/mygithub/MainActivity1.java
+++ app/src/main/java/com/git/mygithub/MainActivity1.java
@@ -1,19 +1,16 @@
 package com.git.mygithub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        qrwreh发文发文各位跟广丰u吧ui暘突然听你
-//        请问请问bdbdbdbd
-// 而问问
-// 爱的个dfghjkl;jhjkh
-// kwgerfwefsddqwfvffvveerverf
-// grgerrer
+//        请问请问bdbdbdb
+//          dsfwgrg3grw
         }
         }
